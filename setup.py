c96dac4338a0d7cb8a93c4bbe6a57de3c1317875
Alex Botello <xxwingcapxx@gmail.com>
2016-11-21 01:08:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,23 @@ except ImportError:
     from distutils.core import setup
 
 with open('requirements.txt', 'r') as r:
-    requires = r.read().split()
+    requirements = r.read().split()
 
 with open('version.txt', 'r') as v:
     version = v.read().strip()
+
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
     name='python-overwatch',
     packages=['overwatch'],
     description=('A Python wrapper for playoverwatch.com'),
+    long_description=readme,
     version=version,
     author='Alexander J. Botello',
     author_email='alexander.botello@g.austincc.edu',
     url='https://github.com/alexbotello/python-overwatch',
-    install_requires=requires,
+    install_requires=requirements,
     license='MIT-Zero'
 )
